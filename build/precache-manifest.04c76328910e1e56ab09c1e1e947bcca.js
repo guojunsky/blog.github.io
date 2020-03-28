9a9f9722bbd024b827f85439623212660202b32f
jun.guo <jun.guo@77hub.com>
2020-03-28 20:58:55 +0800

    feat: init

---
+++ build/precache-manifest.04c76328910e1e56ab09c1e1e947bcca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea00f1a577a6dc9aeefe710d203b6dd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f9c060935762375f207",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4b555e5f964a94106ad",
+    "url": "/static/js/2.9727dc18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9727dc18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9c060935762375f207",
+    "url": "/static/js/main.a1d4d726.chunk.js"
+  },
+  {
+    "revision": "ddefe0e923c5f7ff072c",
+    "url": "/static/js/runtime-main.9adcf9e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
