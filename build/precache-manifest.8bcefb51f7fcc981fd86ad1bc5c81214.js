084cd72beee597bb17eca05a6dcaa95090f94b8c
jun.guo <jun.guo@77hub.com>
2020-03-28 21:05:18 +0800

    fix: 打包路径

---
+++ build/precache-manifest.8bcefb51f7fcc981fd86ad1bc5c81214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958d3f3f3fc4bff1ab743d50b765ad17",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "70aff18ea2ce4ca638fe",
+    "url": "/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4b555e5f964a94106ad",
+    "url": "/build/static/js/2.9727dc18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build/static/js/2.9727dc18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70aff18ea2ce4ca638fe",
+    "url": "/build/static/js/main.2ce66772.chunk.js"
+  },
+  {
+    "revision": "bf0d2ec8436507a92813",
+    "url": "/build/static/js/runtime-main.c19783a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04c76328910e1e56ab09c1e1e947bcca.js"
+  "/build/precache-manifest.8bcefb51f7fcc981fd86ad1bc5c81214.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
